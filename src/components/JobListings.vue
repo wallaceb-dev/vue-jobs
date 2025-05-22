aa5f895eb81c76e5d4cf5bd93fe07bfb042d2bf6
Wallace <wallaceb.teixeira@gmail.com>
2025-05-22 19:10:13 -0300

    feat: :sparkles: make job view page dynamic

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref, defineProps, onMounted } from "vue";
+import { ref, defineProps, onMounted, reactive } from "vue";
 import JobListing from "./JobListing.vue";
 import axios from "axios";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
 defineProps({
     limit: Number,
@@ -11,14 +12,19 @@ defineProps({
     }
 });
 
-const jobs = ref([]);
+const state = reactive({
+    jobs: [],
+    isLoading: true
+});
 
 onMounted(async () => {
     try {
         const response = await axios.get('http://localhost:8000/jobs');
-        jobs.value = response.data;
+        state.jobs = response.data;
     } catch (error) {
         console.error('Error fetching jobs', error);
+    } finally {
+        state.isLoading = false;
     }
 });
 </script>
@@ -27,8 +33,11 @@ onMounted(async () => {
     <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+            <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader />
+            </div>
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
