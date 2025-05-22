40d6717f5e15a3d6672ede89445087503083ab16
Wallace <wallaceb.teixeira@gmail.com>
2025-05-22 12:31:48 -0300

    feat: :building_construction: finish cards' structure

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero title="Test Title" subtitle="Test Subtitle" />
   <HomeCards />
+  <JobListings :limit="3" :showButton="true"/>
 </template>
