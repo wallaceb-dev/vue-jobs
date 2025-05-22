43721e3df85406ebb52312d965a49221879454c8
Wallace <wallaceb.teixeira@gmail.com>
2025-05-22 19:15:50 -0300

    feat: :art: add back button to job view page

--- src/components/JobView.vue
+++ src/components/JobView.vue
@@ -3,6 +3,7 @@ import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import axios from 'axios';
 import { reactive, onMounted } from "vue";
 import { useRoute } from "vue-router";
+import BackButton from "./BackButton.vue";
 
 const route = useRoute();
 
@@ -26,6 +27,7 @@ onMounted(async () => {
 </script>
 
 <template>
+    <BackButton />
     <section v-if="!state.isLoading" class="bg-green-50">
         <div class="container m-auto py-10 px-6">
             <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
@@ -83,7 +85,8 @@ onMounted(async () => {
                     <div class="bg-white p-6 rounded-lg shadow-md mt-6">
                         <h3 class="text-xl font-bold mb-6">Manage Job</h3>
                         <RouterLink :to="`/jobs/edit/${state.job.id}`"
-                            class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">Edit
+                            class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                            Edit
                             Job</RouterLink>
                         <button
                             class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
