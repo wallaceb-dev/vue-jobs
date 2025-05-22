ed05417e9cdefeb7193c10503a027f24bfe86f73
Wallace <wallaceb.teixeira@gmail.com>
2025-05-22 10:57:26 -0300

    feat: :building_construction: implement cards components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
 </script>
 
 <template>
   <Navbar />
-  <Hero />
+  <Hero title="Test Title" subtitle="Test Subtitle" />
+  <HomeCards />
 </template>
